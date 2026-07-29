6f0fc14092f0d45b75156acde661c54929036630
atiqasadia0189 <atiqasadia06@gmail.com>
2026-07-29 23:33:56 +0500

    Create  LoopsPractice.java

---
+++ Loops/LoopsPractice.java
@@ -0,0 +1,20 @@
+public class LoopsPractice{
+ public static void main(String[] args){
+   // For Loop
+   for(int i = 0; i <= 100; i++){
+   System.out.println(i);
+   }
+   // While Loop
+   int j = 0;
+   while(j <= 100){
+   System.out.println(j);
+   j++;
+   }
+   // do-while Loop
+   int k = 0;
+   do {
+     System.out.println(k);
+     k++;
+   } while(k <= 100);
+}
+}
