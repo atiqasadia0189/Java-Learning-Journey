abbf2e0c0f899099ab9004c625ee6eff8672fcee
atiqasadia0189 <atiqasadia06@gmail.com>
2026-07-29 11:21:29 +0500

    Create VariablesPractice.java

---
+++ 00_Variables/VariablesPractice.java
@@ -0,0 +1,19 @@
+// Java Variables and Data Types Practice
+public class VariablesPractice {
+  public static void main(String[] args) {
+    // String Data Type
+  String name = "Atiqa";
+    // Integer Data Type
+  int age = 23;
+    // Double Data Type
+  double score = 95.5;
+    // Character Data Type
+  char grade = 'A';
+    // Boolean Data Type
+  boolean isJavafun = true;
+    // Printing the values
+  System.out.println(name);
+  System.out.println(age); 
+  System.out.println(score);
+  System.out.println(grade);
+  System.out.println(isJavafun);
