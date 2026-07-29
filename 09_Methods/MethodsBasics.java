e9ebe752d39616c7a5d445af681727311bffd2ac
atiqasadia0189 <atiqasadia06@gmail.com>
2026-07-30 00:55:21 +0500

    Create MethodsBasics.java

---
+++ 09_Methods/MethodsBasics.java
@@ -0,0 +1,21 @@
+public class MethodsBasics{
+  public static void printJava(){
+    System.out.println("Hello Java");
+  }
+   public static void printName(String name){
+    System.out.println(name);
+   }
+   public static void printSum(int a, int b){
+    int sum = a + b;
+    System.out.println(sum);
+   }
+ public static void main(String[] args){
+  printJava();
+  printJava();
+
+  printName("Atiqa");
+  printName("Sadia");
+  
+  printSum(7, 5);
+}
+}
