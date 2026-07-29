bcf2354a4e9b9392a721f9b4c5ba04388f9855fb
atiqasadia0189 <atiqasadia06@gmail.com>
2026-07-29 12:44:53 +0500

    Create ArithmeticOperators.java

---
+++ Arithmetic/ArithmeticOperators.java
@@ -0,0 +1,22 @@
+ // Arithmetic Operators
+ public class ArithmeticOperators {
+ public static void main(String[] args){
+    int a = 9;
+    int b = 4;
+    // Add 2 numbers
+    int sum = a + b;
+    System.out.println(sum);
+    // Subtracting 2 numbers
+    int sub = a - b;
+    System.out.println(sub);
+    // Multiply 2 numbers
+    int mul = a * b;
+    System.out.println(mul);
+    // Divide 2 numbers
+    int div = a / b;
+    System.out.println(div);
+    // Remainder of 2 numbers 
+    int modulo = a % b;
+    System.out.println(modulo);
+ }
+}
