913b98f74529fb7ce3e53cda3da3b900a8367d5e
atiqasadia0189 <atiqasadia06@gmail.com>
2026-07-29 13:04:54 +0500

    Update ConditionPractice.java

--- 01_ControlFlow/ConditionPractice.java
+++ 01_ControlFlow/ConditionPractice.java
@@ -1,7 +1,10 @@
-// Java If-Else Conditions Practice
+// Java If-Else Conditions Practice 
 public class ConditionPractice {
   public static void main(String[] args) {
-    int age = 23;
+    int age = 30;
+    int age1 = 40;
+    int age2 = 60;
+    boolean isRaining = true;
     // Check Adult or not adult
   if(age >= 18) {
     System.out.println("Adult");
@@ -9,5 +12,17 @@ public class ConditionPractice {
    else {
      System.out.println("not-adult");
    }
+    // Logical AND Condition
+    if(age < 50 && age1 < 50){
+      System.out.println("Both less than 50");
+    }
+     // Logical OR Condition
+    if(age < 50 || age2 < 50){
+      System.out.println("Atleast one less than 50");
+    }
+    // Logical NOT Condition
+    if(!isRaining){
+      System.out.println("It is not raining");
+    }
   }
 }
