1542df8d0cd286a4010b20f23db42e64e6f50601
atiqasadia0189 <atiqasadia06@gmail.com>
2026-07-29 11:37:49 +0500

    Create ConditionPractice.java

---
+++ 01_ControlFlow/ConditionPractice.java
@@ -0,0 +1,13 @@
+// Java If-Else Conditions Practice
+public class ConditionPractice {
+  public static void main(String[] args) {
+    int age = 23;
+    // Check Adult or not adult
+  if(age >= 18) {
+    System.out.println("Adult");
+  } 
+   else {
+     System.out.println("not-adult");
+   }
+  }
+}
